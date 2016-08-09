90b78b3a8035cb4da3d4ec8df1cf229439e8b441
yokosako <t121068@kaiyodai.ac.jp>
2016-08-09 17:52:02 +0900

    赤色直線検出と切片、角度

--- yama.cc
+++ yama.cc
@@ -1,6 +1,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <math.h>
 #include <iostream>
 
 using namespace cv;
@@ -10,50 +11,36 @@ int main( int argc, char** argv )
 {
   VideoCapture cap(0);
   for(int i=0;;i++)
-   {
+    {
       Mat color_img;
       cap >> color_img;
-
-  Mat gray_img; 
-  // Mat bin_img;
-  // Mat smooth_video;
-  // medianBlur(color_img, smooth_video, 5);
-  // cvtColor(color_img, color_img, CV_RGB2GRAY); 
-  	cvtColor(color_img, gray_img, CV_BGR2HSV);
-	//	imshow("", gray_img);
-		inRange(gray_img, Scalar(150, 70, 70), Scalar(180, 255, 255), gray_img);
-	
-	//	 cvtColor(gray_img, gray_img, CV_RGB2GRAY); 
-
-	//	 GaussianBlur(gray_img, gray_img, Size(17,17), 2, 2);
-	//	 Canny(gray_img, gray_img, 10, 20, 3);
-
-	/*        for(int y=0; y<gray_img.rows;y++){
-            for(int x=0; x<gray_img.cols; x++){
-	      int hue = gray_img.at<Vec3b>(y,x)[0];
- int sat = gray_img.at<Vec3b>(y,x)[1];
- int val = gray_img.at<Vec3b>(y,x)[2];
- if((hue<=160&&hue>=190)&&sat>=150&&val>=0) {         
-   gray_img.at<uchar>(y,x)=255;
- }else{
-   gray_img.at<uchar>(y,x)=0;
-            }
-	    }
+      Mat gray_img;
+      cvtColor(color_img, gray_img, CV_BGR2HSV);
+      inRange(gray_img, Scalar(150, 70, 70), Scalar(180, 255, 255), gray_img);
+      vector<Vec4i> lines;
+      HoughLinesP( gray_img, lines, 1, CV_PI/180, 80, 30, 10 );
+      if(lines.size()>=1){
+	line( color_img, Point(lines[0][0], lines[0][1]),Point(lines[0][2], lines[0][3]), Scalar(255,0,0), 3, 8 );
+	double katamuki =(((double)-lines[0][3]+lines[0][1])/(lines[0][2]-lines[0][0]));
+	line (color_img, Point(color_img.cols/2,0),Point(color_img.cols/2,color_img.rows),Scalar(0,0,0),2,8);
+	line (color_img, Point(0,color_img.rows/2),Point(color_img.cols,color_img.rows/2),Scalar(0,0,0),2,8);
+	double seppen;
+	seppen = color_img.rows+(-1)*(katamuki*lines[0][0]+lines[0][1]);
+	double seppen2;
+	seppen2 = katamuki*color_img.cols+seppen;
+	double seppen3;
+	seppen3 = (-1)*((seppen2+seppen)/2-color_img.rows/2)/katamuki;
+	//cout << "x軸切片"<<seppen3 <<  "y軸切片"<<(seppen2+seppen)/2-color_img.rows/2  <<endl;
+	//cout << "x軸切片"<<seppen3 <<endl;
+	//cout << "y軸切片"<<(seppen2+seppen)/2-color_img.rows/2  << endl;
+	if(katamuki>=0){
+	  cout << "角度:"<<atan(katamuki)*(180/CV_PI) <<endl;
+	}else{
+	  cout << "角度:"<<180+atan(katamuki)*(180/CV_PI) <<endl;
 	}
-		imshow("",gray_img);	*/	 
-      
-	       
- 
-  vector<Vec4i> lines;
-    HoughLinesP( gray_img, lines, 1, CV_PI/180, 80, 30, 10 );
-    for( size_t i = 0; i < lines.size(); i++ )
-    {
-        line( color_img, Point(lines[i][0], lines[i][1]),
-            Point(lines[i][2], lines[i][3]), Scalar(255,0,0), 3, 8 );
+      }
+      imshow( "ライン検出", color_img );
+      waitKey(50);
     }
-
-    imshow( "ライン検出", color_img );
-  waitKey(50);
-   }
   return 0;
 }
